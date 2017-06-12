38d869498aa045c035251a543a5e6c255323fd92
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2017-06-12 23:40:38 +0200

    adding request

--- app/src/main/java/com/example/automediabsico/ServicioMusicBrowserTest.java
+++ app/src/main/java/com/example/automediabsico/ServicioMusicBrowserTest.java
@@ -12,6 +12,16 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.service.media.MediaBrowserService;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,68 +34,25 @@ public class ServicioMusicBrowserTest extends MediaBrowserService {
     private MediaMetadata mCurrentTrack;
     private int currentIndex = 0;
 
+    private final String TAG = ServicioMusicBrowserTest.this.getClass().getSimpleName();
+    private final String URL = "http://storage.googleapis.com/automotive-media/music.json";
+    private RequestQueue requestQueue;
+
+    private Gson gson;
+    private Musica musica;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        mMusic = new ArrayList<MediaMetadata>(); //Añadimos 3 canciones desde la librería de audio de youtube
-        mMusic.add(new MediaMetadata.Builder().putString(MediaMetadata.METADATA_KEY_MEDIA_ID, "https://www.youtube.com/audiolibrary_download?vid=f5cfb6bd8c048b98").putString(MediaMetadata.METADATA_KEY_TITLE, "Primera canción").putString(MediaMetadata.METADATA_KEY_ARTIST, "Artista 1").putLong(MediaMetadata.METADATA_KEY_DURATION, 109000).build());
-        mMusic.add(new MediaMetadata.Builder().putString(MediaMetadata.METADATA_KEY_MEDIA_ID, "https://www.youtube.com/audiolibrary_download?vid=ac7a38f4a568229c").putString(MediaMetadata.METADATA_KEY_TITLE, "Segunda canción").putString(MediaMetadata.METADATA_KEY_ARTIST, "Artista 2").putLong(MediaMetadata.METADATA_KEY_DURATION, 65000).build());
-        mMusic.add(new MediaMetadata.Builder().putString(MediaMetadata.METADATA_KEY_MEDIA_ID, "https://www.youtube.com/audiolibrary_download?vid=456229530454affd").putString(MediaMetadata.METADATA_KEY_TITLE, "Tercera canción").putString(MediaMetadata.METADATA_KEY_ARTIST, "Artista 3").putLong(MediaMetadata.METADATA_KEY_DURATION, 121000).build());
-        mPlayer = new MediaPlayer();
-        mSession = new MediaSession(this, "MiServicioMusical");
-        mSession.setCallback(new MediaSession.Callback() {
-            @Override
-            public void onPlayFromMediaId(String mediaId, Bundle extras) {
-                for (MediaMetadata item : mMusic) {
-                    if (item.getDescription().getMediaId().equals(mediaId)) {
-                        mCurrentTrack = item;
-                        break;
-                    }
-                }
-                //
-                handlePlay();
-            }
 
-            @Override
-            public void onPlay() {
-                if (mCurrentTrack == null) {
-                    mCurrentTrack = mMusic.get(currentIndex);
-                    handlePlay();
-                } else {
-                    mPlayer.start();
-                    mSession.setPlaybackState(buildState(PlaybackState.STATE_PLAYING));
-                }
-            }
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
 
-            @Override
-            public void onPause() {
-                mPlayer.pause();
-                mSession.setPlaybackState(buildState(PlaybackState.STATE_PAUSED));
-            }
+        requestQueue = Volley.newRequestQueue(this);
 
-            @Override
-            public void onSkipToNext() {
-                if (currentIndex < mMusic.size()-1) {
-                    currentIndex++;
-                    mCurrentTrack = mMusic.get(currentIndex);
-                    mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_NEXT));
-                    handlePlay();
-                }
-            }
+        getRepositorioMusical();
 
-            @Override
-            public void onSkipToPrevious() {
-                if (currentIndex > 0) {
-                    currentIndex--;
-                    mCurrentTrack = mMusic.get(currentIndex);
-                    mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_PREVIOUS));
-                    handlePlay();
-                }
-            }
-        });
-        mSession.setFlags(MediaSession.FLAG_HANDLES_MEDIA_BUTTONS | MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS);
-        mSession.setActive(true);
-        setSessionToken(mSession.getSessionToken());
+         mMusic = new ArrayList<>();
     }
 
     private PlaybackState buildState(int state) {
@@ -94,29 +61,31 @@ public class ServicioMusicBrowserTest extends MediaBrowserService {
     }
 
     private void handlePlay() {
-        mPlayer.seekTo(0);
-        mSession.setPlaybackState(buildState(PlaybackState.STATE_PLAYING));
-        mSession.setMetadata(mCurrentTrack);
-        try {
-            mPlayer.reset();
-            mPlayer.setDataSource(ServicioMusicBrowserTest.this, Uri.parse(mCurrentTrack.getDescription().getMediaId()));
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(mPlayer.isPlaying()){
+            mPlayer.seekTo(0);
+            mSession.setPlaybackState(buildState(PlaybackState.STATE_PLAYING));
+            mSession.setMetadata(mCurrentTrack);
+            try {
+                mPlayer.reset();
+                mPlayer.setDataSource(ServicioMusicBrowserTest.this, Uri.parse(mCurrentTrack.getDescription().getMediaId()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+            });
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.seekTo(0);
+                    mSession.setPlaybackState(buildState(PlaybackState.STATE_PAUSED));
+                }
+            });
+            mPlayer.prepareAsync();
         }
-        mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.start();
-            }
-        });
-        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                mediaPlayer.seekTo(0);
-                mSession.setPlaybackState(buildState(PlaybackState.STATE_PAUSED));
-            }
-        });
-        mPlayer.prepareAsync();
     }
 
     @Override
@@ -137,4 +106,104 @@ public class ServicioMusicBrowserTest extends MediaBrowserService {
     public void onDestroy() {
         mSession.release();
     }
+
+
+    private void getRepositorioMusical() {
+        StringRequest request = new StringRequest(Request.Method.GET, URL, onPostsLoaded, onPostsError);
+        requestQueue.add(request);
+    }
+
+    private final Response.Listener<String> onPostsLoaded = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            musica = gson.fromJson(response, Musica.class);
+            Log.d(TAG, "Número de pistas de audio: " + musica.getMusica().size());
+
+            int slashPos = URL.lastIndexOf('/');
+            String path = URL.substring(0, slashPos + 1);
+
+            for (int i = 0; i < musica.getMusica().size(); i++) {
+                PistaAudio pista = musica.getMusica().get(i);
+                if (!pista.getSource().startsWith("http")) {
+                    pista.setSource(path + pista.getSource());
+                }
+
+                if (!pista.getImage().startsWith("http")) {
+                    pista.setImage(path + pista.getImage());
+                }
+
+                mMusic.add(new MediaMetadata.Builder()
+                        .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, pista.getSite())
+                        .putString(MediaMetadata.METADATA_KEY_TITLE, pista.getTitle())
+                        .putString(MediaMetadata.METADATA_KEY_ARTIST, pista.getArtist())
+                        .putLong(MediaMetadata.METADATA_KEY_DURATION, pista.getDuration() * 1000)
+                        .build());
+
+                // musica.getMusica().set(i, pista);
+            }
+
+            mPlayer = new MediaPlayer();
+            mSession = new MediaSession(ServicioMusicBrowserTest.this, "MiServicioMusical");
+            mSession.setCallback(new MediaSession.Callback() {
+                @Override
+                public void onPlayFromMediaId(String mediaId, Bundle extras) {
+                    for (MediaMetadata item : mMusic) {
+                        if (item.getDescription().getMediaId().equals(mediaId)) {
+                            mCurrentTrack = item;
+                            break;
+                        }
+                    }
+                    handlePlay();
+                }
+
+                @Override
+                public void onPlay() {
+                    if (mCurrentTrack == null) {
+                        mCurrentTrack = mMusic.get(currentIndex);
+                        handlePlay();
+                    } else {
+                        mPlayer.start();
+                        mSession.setPlaybackState(buildState(PlaybackState.STATE_PLAYING));
+                    }
+                }
+
+                @Override
+                public void onPause() {
+                    if(mPlayer.isPlaying()){
+                        mPlayer.pause();
+                        mSession.setPlaybackState(buildState(PlaybackState.STATE_PAUSED));
+                    }
+                }
+
+                @Override
+                public void onSkipToNext() {
+                    if (currentIndex < mMusic.size() - 1) {
+                        currentIndex++;
+                        mCurrentTrack = mMusic.get(currentIndex);
+                        mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_NEXT));
+                        handlePlay();
+                    }
+                }
+
+                @Override
+                public void onSkipToPrevious() {
+                    if (currentIndex > 0) {
+                        currentIndex--;
+                        mCurrentTrack = mMusic.get(currentIndex);
+                        mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_PREVIOUS));
+                        handlePlay();
+                    }
+                }
+            });
+            mSession.setFlags(MediaSession.FLAG_HANDLES_MEDIA_BUTTONS | MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS);
+            mSession.setActive(true);
+            setSessionToken(mSession.getSessionToken());
+        }
+    };
+    private final Response.ErrorListener onPostsError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e(TAG, error.toString());
+        }
+    };
 }
